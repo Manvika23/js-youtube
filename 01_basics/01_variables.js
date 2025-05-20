20e4df755103ea57436efa91c46693e8b3cf5e0f
Manvika23 <manvikaag2005@gmail.com>
2025-05-20 17:39:45 +0000

    container.config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail="manvi23@gnail.com"
+var accountPassword="12345"
+accountCity ="jaipur"
+console.log(accountId);
+accountEmail="harar@gam"
+accountPassword="122456"
+accountCity="gwalior"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+prefer not to use var
+issue of block scope and functional scope
+*/
