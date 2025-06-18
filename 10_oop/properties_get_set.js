1191eb37f6efb99e2fd0308d51ca647904341452
Manvika23 <manvikaag2005@gmail.com>
2025-06-18 17:57:07 +0000

    object

--- 10_oop/properties_get_set.js
+++ 10_oop/properties_get_set.js
@@ -1,1 +1,17 @@
-function User(email,password)
+function User(email,password){
+    this._email = email;
+    this._password=password
+
+
+    Object.defineProperty(this,'email',{
+        get:function(){
+            return this._email.toUpperCase()
+        },
+        set:function(){
+            this.email=value
+        }
+    })
+}
+
+const chai = new User("chahudb@","manvi")
+console.log(manvi.email);
